9f919fc5fc61968aac55c47a8c5763cfe987c083
Waaiez Kinnear <43832467+Waaiez@users.noreply.github.com>
2022-05-18 16:23:40 +0200

    remove anitomyscript npm, use browser version

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Script src='/anitomyscript/anitomyscript.bundle.min.js' />
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
