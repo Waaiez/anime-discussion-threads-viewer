754bd915eb8dd0e80ece0ee9fb9dba9e3ce4d26f
Waaiez Kinnear <43832467+Waaiez@users.noreply.github.com>
2022-05-18 10:16:13 +0200

    add components to index page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,22 @@
+import Head from 'next/head';
+
+import { RecentThreads, Search, TrendingAnime } from '../components';
+
 export default function Home() {
-	return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+	return (
+		<>
+			<Head>
+				<title>Anime Discussion Threads</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<div className='bg-gray-800 min-h-screen flex flex-col items-center justify-around select-none'>
+				<div className='max-w-4xl mx-auto w-full px-20 2xl:max-w-7xl h-full justify-evenly flex flex-col'>
+					<Search />
+					<RecentThreads />
+					<TrendingAnime />
+				</div>
+			</div>
+		</>
+	);
 }
