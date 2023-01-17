7cd881c02f9660505a8f0a8921cab5212cbe1184
Waaiez Kinnear <waaiez.kinnear10@gmail.com>
2023-01-17 14:53:23 +0200

    create universal button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import { Search, ArrowRightS, ArrowLeftS } from '@steeze-ui/remix-icons';
+
+	export let icon: 'rightChevron' | 'leftChevron' | 'search';
+	export let colour: 'primary' | 'secondary';
+	export let link: string;
+
+	let iconMapping = {
+		rightChevron: ArrowRightS,
+		leftChevron: ArrowLeftS,
+		search: Search
+	};
+</script>
+
+<a href={link} class={`btn btn-ghost-${colour} btn-base rounded-lg w-full !ring-0`}>
+	<Icon src={iconMapping[icon]} size="40" />
+</a>
