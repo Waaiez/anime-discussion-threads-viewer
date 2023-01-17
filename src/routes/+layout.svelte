a4bd6dab42f365166b8f57c656731c5049a956a3
Waaiez Kinnear <waaiez.kinnear10@gmail.com>
2023-01-17 14:54:41 +0200

    add layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,15 @@
-<script>
-	import '@brainandbones/skeleton/themes/theme-skeleton.css';
-	import '@brainandbones/skeleton/styles/all.css';
-	import '@brainandbones/skeleton/styles/tailwind.css';
-	import '@brainandbones/skeleton/styles/core.css';
-	import '@brainandbones/skeleton/styles/typography.css';
-	import '@brainandbones/skeleton/styles/forms.css';
-	import '@brainandbones/skeleton/styles/elements.css';
-	import { Toast } from '@brainandbones/skeleton';
+<script lang="ts">
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+
+	import { AppShell, Toast } from '@skeletonlabs/skeleton';
 </script>
 
-<svelte:head>
-	<title>Anime Discussion Thread Viewer</title>
-</svelte:head>
+<Toast />
 
-<Toast background="bg-accent-500" position="tr" variant="filled" duration={250} />
-
-<div
-	class="flex flex-col items-center min-h-screen select-none w-full max-w-4xl p-7 mx-auto md:px-20 2xl:max-w-7xl justify-center gap-5"
->
-	<slot />
-</div>
+<AppShell>
+	<div class="max-w-3xl md:max-w-5xl mx-auto py-16 px-6 lg:px-8 select-none flex flex-col h-full">
+		<slot />
+	</div>
+</AppShell>
