f329d1998ed91370d5299a38d168f1a51e7d7901
Waaiez Kinnear <waaiez.kinnear10@gmail.com>
2023-01-17 15:18:49 +0200

    create skeleton card

--- src/lib/components/SkeletonCard.svelte
+++ src/lib/components/SkeletonCard.svelte
@@ -1,12 +1,8 @@
-<li class="relative cursor-auto animate-pulse select-none">
-	<div class="bg-transparent">
-		<div class="shadow-xl">
-			<div class="aspect-w-2 aspect-h-3 rounded-lg bg-accent-300/50" />
-		</div>
-		<div class="select-none space-y-2">
-			<p class="w-3/4 h-4 rounded-lg bg-accent-300/50 mt-2" />
-			<div class="w-2/4 h-4 rounded-lg bg-accent-300/50" />
-			<div class="w-1/4 h-4 rounded-lg bg-accent-300/50" />
-		</div>
+<div class="relative overflow-hidden animate-pulse">
+	<div class="h-96 w-56 bg-surface-900 rounded-xl" />
+	<div class="space-y-2">
+		<div class="w-3/4 h-4 rounded-xl bg-surface-900 mt-4" />
+		<div class="w-2/4 h-4 rounded-xl bg-surface-900" />
+		<div class="w-1/4 h-4 rounded-xl bg-surface-900" />
 	</div>
-</li>
+</div>
