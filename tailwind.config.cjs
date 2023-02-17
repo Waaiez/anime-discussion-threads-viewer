c3c9ab56838db452a3960e514e074fba411942ae
Waaiez Kinnear <waaiez.kinnear10@gmail.com>
2023-02-17 15:26:09 +0200

    add search page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -29,6 +29,7 @@ module.exports = {
 		}
 	},
 	plugins: [
+		require('@tailwindcss/forms'),
 		require('@tailwindcss/aspect-ratio'),
 		require('@tailwindcss/typography'),
 		require('@tailwindcss/line-clamp'),
